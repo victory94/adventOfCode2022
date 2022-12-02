a08790b407b8d711712f5087919485091761f188
Viktoria <viktoria_a94@hotmail.com>
2022-12-02 20:09:47 +0100

    Removed method for use of map instead

--- src/main/java/org/example/Day2.java
+++ src/main/java/org/example/Day2.java
@@ -5,12 +5,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day2 {
     public  ArrayList<String> winnig_combo = new ArrayList<>(Arrays.asList("AY", "BZ", "CX"));
     public ArrayList<String> losing_combo = new ArrayList<>(Arrays.asList("AZ", "BX", "CY"));
     public ArrayList<String> draw_combo = new ArrayList<>(Arrays.asList("AX", "BY", "CZ"));
     public static void main(String[] args) throws IOException {
+        Map<String, Integer> choice_map = Stream.of(new Object[][] {
+                { "Z", 3 },
+                { "Y", 2 },
+                { "X", 1 },
+        }).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
         Day2 instance = new Day2();
         InputStream is = instance.getFileAsIOStream("input_day2.txt");
         InputStreamReader isr = new InputStreamReader(is);
@@ -23,11 +32,11 @@ public class Day2 {
             //Empty line rock paper scissors
             if(!line.isEmpty()){
                 String[] choices = line.trim().split("\\s+");
-                score = score + instance.game_score(choices[0], choices[1]);
-                score = score + instance.getScoreChoice(choices[1]);
+                score = score + instance.game_score(choices[0] + choices[1]);
+                score = score + choice_map.get(choices[1]);
                 String new_choice = instance.get_end_choice(choices[0], choices[1]);
-                score2 = score2 + instance.getScoreChoice(new_choice);
-                score2 = score2 + instance.game_score(choices[0], new_choice);
+                score2 = score2 + choice_map.get(new_choice);
+                score2 = score2 + instance.game_score(choices[0] + new_choice);
             }
 
         }
@@ -49,18 +58,8 @@ public class Day2 {
         return ioStream;
     }
 
-    private int getScoreChoice(String a){
-        if(a.compareTo("Z") == 0){
-            return 3;
-        }else if(a.compareTo("Y") == 0){
-            return 2;
-        }else{
-            return 1;
-        }
-    }
 
-    private int game_score(String elf, String challenger){
-        String check = elf + challenger;
+    private int game_score(String check){
         if(winnig_combo.contains(check)){
            return 6;
         }else if(losing_combo.contains(check)){
